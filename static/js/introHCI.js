74ddd173006dd2b7433839f2b257bc4b78d77535
Zhaoshuo Bi <zbi@ucsd.com>
2018-01-25 10:31:14 -0800

    add

--- static/js/introHCI.js
+++ static/js/introHCI.js
@@ -10,9 +10,30 @@ $(document).ready(function() {
  */
 function initializePage() {
 	$("#testjs").click(function(e) {
-		$('.jumbotron h1').text("Javascript is connected");
+		$('.jumbotron h1').text("Great!!!!! You got an A");
+		$("#testjs").text("Please wait...");
+	//	$(".jumbotron p").addClass("active");
+		$(".jumbotron p").toggleClass("active");
+
 	});
 
 	// Add any additional listeners here
+	$("a.thumbnail").click(projectClick);
 	// example: $("#div-id").click(functionToCall);
 }
+function projectClick(e) { 
+    // prevent the page from reloading      
+    e.preventDefault();
+    // In an event handler, $(this) refers to      
+    // the object that triggered the event      
+    $(this).css("background-color", "#7fff00");
+    var containingProject = $(this).closest(".project");
+ 	var containingProject = $(this).closest(".project");
+    var description = $(containingProject).find(".project-description");
+    if (description.length == 0) {
+       $(containingProject).append("<div class='project-description'><p>Description of the project.</p></div>");
+    } else {
+       //description.html("<p>Stop clicking on me! You just did it at " + (new Date()) + "</p>");
+       $(containingProject).fadeOut("<div class='project-description'><p>Description of the project.</p></div>");
+
+    }}
